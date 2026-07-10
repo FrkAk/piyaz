22fddf7f001504f1a0926a689f2917afb9a74912
Furkan Akbulutlar <f.akbulutlar@gmail.com>
2026-07-11 01:33:28 +0200

    feat: add shared_since column, note event check, partial note index

--- drizzle/0007_previous_major_mapleleaf.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "activity_events" ADD COLUMN "note_id" uuid;--> statement-breakpoint
-ALTER TABLE "activity_events" ADD CONSTRAINT "activity_events_note_id_notes_id_fk" FOREIGN KEY ("note_id") REFERENCES "public"."notes"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
-CREATE INDEX "activity_events_note_id_created_idx" ON "activity_events" USING btree ("note_id","created_at");
